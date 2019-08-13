0240a4687d6d31f6ba2b9b9d6beee8dc9f0ac5a6
jmaicaaan <johnmichaelubas.santos@gmail.com>
2019-08-13 18:48:48 +0800

    :sparkles: Implements random button colors on page start
    Co-authored-by: Julie <julieheartcrochet@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-let colors = ['#9AD1D4', '#62C370', '#CC3363', '#0E0319'];
+const colors =  ['rgb(154, 209, 212)', 'rgb(98, 195, 112)', 'rgb(204, 51, 99)', 'rgb(14, 3, 25)']
+const htmlPlayButton = document.querySelector('#play_button');
+
+function main() {
+    window.addEventListener('load', handleOnLoad);
+}
+
+function handleOnLoad() {
+    setInterval(() => runRandomButton(), 2500)
+}
+
+function runRandomButton() {
+    const randomNumber = getRandomNumber();
+    const currentColor = htmlPlayButton.style.backgroundColor;
+    const color = colors[randomNumber];
+
+    if (currentColor === color) {
+      return runRandomButton()
+    }
+    htmlPlayButton.style.backgroundColor = color;
+}
+
+function getRandomNumber() {
+    return Math.floor((Math.random() * 3) + 1);
+}
+
+main();

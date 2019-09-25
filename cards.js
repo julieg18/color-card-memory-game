45958ea88b6d4032f6ddd4949284d9c9bb318b2e
jmaicaaan <johnmichaelubas.santos@gmail.com>
2019-09-25 19:01:36 +0800

    :recycle: - Card - Implements flipping of card. Closes #1
    Co-authored-by: Julie <julieheartcrochet@gmail.com>

--- cards.js
+++ cards.js
@@ -1,10 +1,37 @@
-const cards = document.querySelectorAll('.card-item');
+const cards = document.querySelectorAll('.scene');
 const exampleColor = 'green';
+const FLIPPED_CSS_CLASS = 'is-flipped';
+const MAXIMUM_FLIPPED_CARDS = 2;
 
 function flipCard(event) {
-  // we need to remove the card-item-flipped later...
-  event.target.classList.add('card-item-flipped');
-  event.target.style.backgroundColor = exampleColor;
+  // TODO - Check other flipped cards when selecting for the 3rd card to flip
+  const element = event.target;
+  const isFlipped = element.classList.contains(FLIPPED_CSS_CLASS);
+  const flippedCounts = getFlippedCards();
+
+  if (flippedCounts === MAXIMUM_FLIPPED_CARDS) {
+    unflipAllCards();
+  }
+
+  if (isFlipped) {
+    event.target.classList.remove(FLIPPED_CSS_CLASS);
+  } else {
+    event.target.classList.add(FLIPPED_CSS_CLASS);
+  }
+}
+
+function getFlippedCards() {
+  return Array.from(cards)
+    .filter(card => {
+      console.log('card.classList', card.classList);
+      card.classList.contains(FLIPPED_CSS_CLASS);
+    });
+}
+
+function unflipAllCards() {
+  Array.from(cards).forEach(card => {
+    card.classList.remove(FLIPPED_CSS_CLASS);
+  })
 }
 
 cards.forEach(card => {

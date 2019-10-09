3d5837767889dbc56640026cd124388c05b45438
julieg18 <julieheartcrochet@gmail.com>
2019-10-09 06:05:08 -0500

    worked on dynamically creating the cards

--- cards.js
+++ cards.js
@@ -1,7 +1,22 @@
 const cards = document.querySelectorAll('.scene');
+const renderCardsButton = document.querySelector('.render-cards-btn');
 const exampleColor = 'green';
 const FLIPPED_CSS_CLASS = 'is-flipped';
 const MAXIMUM_FLIPPED_CARDS = 2;
+let cardArray = [
+  {
+    backColor: 'green',
+    id: 1
+  },
+  {
+    backColor: 'orange',
+    id: 2
+  },
+  {
+    backColor: 'red',
+    id: 3
+  }
+];
 
 function flipCard(event) {
   // TODO - Check other flipped cards when selecting for the 3rd card to flip
@@ -21,19 +36,43 @@ function flipCard(event) {
 }
 
 function getFlippedCards() {
-  return Array.from(cards)
-    .filter(card => {
-      console.log('card.classList', card.classList);
-      card.classList.contains(FLIPPED_CSS_CLASS);
-    });
+  return Array.from(cards).filter(card => {
+    console.log('card.classList', card.classList);
+    card.classList.contains(FLIPPED_CSS_CLASS);
+  });
 }
 
 function unflipAllCards() {
   Array.from(cards).forEach(card => {
     card.classList.remove(FLIPPED_CSS_CLASS);
-  })
+  });
+}
+
+function createCard() {
+  const scene = document.createElement('div');
+  const card = document.createElement('div');
+  const frontCard = document.createElement('div');
+  const backCard = document.createElement('div');
+
+  scene.classList.add('scene');
+  card.classList.add('card');
+  frontCard.classList.add('card__face', 'card__face--front');
+  backCard.classList.add('card__face', 'card__face--back');
+
+  card.appendChild(frontCard);
+  card.appendChild(backCard);
+
+  scene.appendChild(card);
+
+  document.querySelector('.cards-container').appendChild(scene);
+}
+
+function renderCards(event) {
+  createCard();
 }
 
 cards.forEach(card => {
   card.addEventListener('click', flipCard);
 });
+
+renderCardsButton.addEventListener('click', renderCards);
